cff4ab7fe6d9e9577c1be4554d6d090e4e50a83b
kimjaehu <kimjaehu@gmail.com>
2019-02-22 02:11:50 +0000

    commits all files after reinstalling of ubuntu

---
+++ mongo_example.js
@@ -0,0 +1,29 @@
+"use strict";
+
+const MongoClient = require("mongodb").MongoClient;
+const MONGODB_URI = "mongodb://localhost:27017/tweeter";
+
+MongoClient.connect(MONGODB_URI, (err, db) => {
+  if (err) {
+    console.error(`Failed to connect: ${MONGODB_URI}`);
+    throw err;
+  }
+
+  // ==> We have a connection to the "test-tweets" db,
+  //     starting here.
+  console.log(`Connected to mongodb: ${MONGODB_URI}`);
+
+  // ==> Let's "get all the tweets". In Mongo-speak, we "find" them.
+  db.collection("tweets").find({}, (err, results) => {
+    if (err) throw err;
+
+    // ==> So we Read The Fantastic Manual, right?
+
+    // ==> We can iterate on the cursor to get results, one at a time:
+    console.log("for each item yielded by the cursor:");
+    results.each((err, item) => console.log("  ", item));
+
+    // This is the end...
+    db.close();
+  });
+});
--- public/scripts/app.js
+++ public/scripts/app.js
@@ -4,75 +4,57 @@
  * Reminder: Use (and do all your DOM work in) jQuery's document ready function
  */
 
-// Test / driver code (temporary). Eventually will get this from the server.
-const data = [
-  {
-    "user": {
-      "name": "Newton",
-      "avatars": {
-        "small":   "https://vanillicon.com/788e533873e80d2002fa14e1412b4188_50.png",
-        "regular": "https://vanillicon.com/788e533873e80d2002fa14e1412b4188.png",
-        "large":   "https://vanillicon.com/788e533873e80d2002fa14e1412b4188_200.png"
-      },
-      "handle": "@SirIsaac"
-    },
-    "content": {
-      "text": "If I have seen further it is by standing on the shoulders of giants"
-    },
-    "created_at": 1461116232227
-  },
-  {
-    "user": {
-      "name": "Descartes",
-      "avatars": {
-        "small":   "https://vanillicon.com/7b89b0d8280b93e2ba68841436c0bebc_50.png",
-        "regular": "https://vanillicon.com/7b89b0d8280b93e2ba68841436c0bebc.png",
-        "large":   "https://vanillicon.com/7b89b0d8280b93e2ba68841436c0bebc_200.png"
-      },
-      "handle": "@rd" },
-    "content": {
-      "text": "Je pense , donc je suis"
-    },
-    "created_at": 1461113959088
-  },
-  {
-    "user": {
-      "name": "Johann von Goethe",
-      "avatars": {
-        "small":   "https://vanillicon.com/d55cf8e18b47d4baaf60c006a0de39e1_50.png",
-        "regular": "https://vanillicon.com/d55cf8e18b47d4baaf60c006a0de39e1.png",
-        "large":   "https://vanillicon.com/d55cf8e18b47d4baaf60c006a0de39e1_200.png"
-      },
-      "handle": "@johann49"
-    },
-    "content": {
-      "text": "Es ist nichts schrecklicher als eine tätige Unwissenheit."
-    },
-    "created_at": 1461113796368
-  }
-];
+function loadTweets() {
+  return $.ajax('/tweets', { method: 'GET' })
+  return tweets;
+}
 
+function postNewTweet() {
+  
 
-// Test / driver code (temporary)
-//console.log($tweet); // to see what it looks like
-//$('#tweets-container').append($tweet); // to add it to the page so we can make sure it's got all the right elements, classes, etc.
+}
 
+function loadNewTweet() {
+  return $.ajax('/tweets', { method: 'GET' })
+  return tweets[tweets.length];
+}
 
-    
 function renderTweets(tweets) {
   // loops through tweets
     // calls createTweetElement for each tweet
     // takes return value and appends it to the tweets container
-  for (tweet in tweets) {
+    for (tweet in tweets) {
     let tweetHtml = createTweetElement(tweets[tweet]);
-    $('#tweets-container').append(tweetHtml);
-  }
-  
+    $('#tweets-container').prepend(tweetHtml);
+  }  
 }
+
+function escape(str) {
+  var div = document.createElement('div');
+  div.appendChild(document.createTextNode(str));
+  return div.innerHTML;
+}
+
 
 function createTweetElement(tweet) {
   // let $tweet = $('<article>').addClass('tweet');
   // ...
+  // let articleElm = document.createElement('article');
+  // let headerElm = document.createElement('header');
+  // let imgElm = document.createElement('img');
+  // let spanElm = document.createElement('span');
+
+  // articleElm.appendChild(headerElm);
+  // headerElm.classList.add("message-header");
+  // headerElm.appendChild(imgElm);
+  // imgElm.classList.add("message-logo");
+  // imgElm.attr("src", tweet.user.avatars.small)
+  // imgElm.append(spanElm);
+  // spanElm.classList.add("message-id");
+  // spanElm.add(document.createTextNode(tweet.user.avatars.small))
+
+  // console.log(imgElm)  
+  // return imgElm;
 
   let $tweet = 
   `
@@ -80,14 +62,14 @@ function createTweetElement(tweet) {
     <header class="message-header">
     <img class="message-logo" src=${tweet.user.avatars.small}></img>
     <span class="message-name">
-      ${tweet.user.name}
+      ${escape(tweet.user.name)}
     </span>
     <span class="message-id">
-    ${tweet.user.handle}
+    ${escape(tweet.user.handle)}
     </span>
     </header>
     <div class="message-body">
-    ${tweet.content.text}
+    ${escape(tweet.content.text)}
     </div>
     <footer class="message-footer">
       <div>${Date.now() - tweet.created_at}</div>
@@ -98,5 +80,43 @@ function createTweetElement(tweet) {
 }
 
 $(document).ready(function() {
-  renderTweets(data);
+  const dataPromise = loadTweets();
+  dataPromise.then(function(tweetData){
+    renderTweets(tweetData);
+  });
+  $('form').submit(function () {
+    if ($('.text-area').val().length > 140){
+      $('.error').text("Error: Tweet exceeds 140 characters!")
+      $('.error').show(50)
+    } else if (!$('.text-area').val()) {
+      $('.error').text("Error: Tweet message not found!")
+      $('.error').show(50)      
+    } else {
+      
+      $.ajax({
+        url:"/tweets",
+        type: 'POST',
+        data: $(this).serialize()
+      }).then(function(){
+        return loadNewTweet()
+      }).then(function(tweetData){
+        renderTweets(tweetData)
+      });
+
+
+      // const dataPromise = loadNewTweet();
+      // dataPromise.then(function(tweetData){
+      // renderTweets(tweetData);
+      // });
+      $('.error').hide(50)
+    }
+    event.preventDefault();
+  });
+  
+  $('#compose').click(function () {
+    $('.new-tweet').slideToggle();
+    $('.text-area').focus();
+    $('text-area').select();
+  });
+
 });
